36d4beeb71a452b7aa1a216a72fb4efa4c01feb5
Noelle Rolwes <nrolwes@gmail.com>
2018-09-21 01:14:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word (phrase)
+  return_array = []
   new_array = phrase.split("")
   new_array.each do |x|
-    puts x.reverse!
+    return_array << x.reverse!
   end
+  return_array
 end 
